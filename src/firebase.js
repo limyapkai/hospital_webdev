48ce7665dbbe65506c539013601cd5cb76388a07
limyapkai@gmail.com <limyapkai@gmail.com>
2021-10-11 21:32:14 +0800

    updated API to ZhiJing's Account

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,14 @@ import {initializeApp } from 'firebase/app';
 //import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbw58QBWr-i_n_rNGZo-f3XXhqVWVHHdk",
-  authDomain: "hospitalprocurementbt3103.firebaseapp.com",
-  databaseURL: "https://hospitalprocurementbt3103-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "hospitalprocurementbt3103",
-  storageBucket: "hospitalprocurementbt3103.appspot.com",
-  messagingSenderId: "460777037982",
-  appId: "1:460777037982:web:b6288b26db013afa645b8f",
-  measurementId: "G-SFB9TEVJFN"
-  };
+  apiKey: "AIzaSyB1OVt7lf1oH3g1fO6rqsKVXU8tfK0jeQg",
+  authDomain: "bt3103project-b69fb.firebaseapp.com",
+  projectId: "bt3103project-b69fb",
+  storageBucket: "bt3103project-b69fb.appspot.com",
+  messagingSenderId: "812445459565",
+  appId: "1:812445459565:web:5906c3c6112b56b7718b64",
+  measurementId: "G-61XJMJD53T"
+};
   
   // Initialize Firebase
   const firebaseApp = initializeApp(firebaseConfig);
